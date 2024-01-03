3f4229ab81c9af55a47cf622692eec622b9ff290
Lena Araújo <lenaaraujo92@gmail.com>
2024-01-02 22:03:13 -0300

    html and js - initial setup

---
+++ receitas.js
@@ -0,0 +1,41 @@
+let receitas = {
+  "bife com panzanella": {
+    ingredientes: [
+      "contrafilé",
+      "pão ciabatta",
+      "abobrinha",
+      "vagem holandesa",
+      "alho",
+      "vinagre balsâmico",
+      "salsinha",
+      "azeite",
+      "sal",
+      "pimenta-do-reino",
+    ],
+  },
+  "peixe com batata e espinafre": {
+    ingredientes: [
+      "robalo",
+      "batata bolinha",
+      "espinafre",
+      "alho",
+      "cúrcuma",
+      "azeite",
+      "sal",
+      "pimenta-do-reino",
+    ],
+  },
+  "bisteca com feijão-branco e brócolis": {
+    ingredientes: [
+      "bisteca de porco",
+      "feijão-branco",
+      "brócolis",
+      "alho",
+      "páprica doce",
+      "alecrim",
+      "azeite",
+      "sal",
+      "pimenta-do-reino",
+    ],
+  },
+};
